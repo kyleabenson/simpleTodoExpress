2478967c40e20566e9327045cf651d65c2dd9bd5
Kyle Benson <kyleabenson@gmail.com>
2017-04-27 15:31:04 -0400

    clean up comments in server.js

--- server.js
+++ server.js
@@ -7,11 +7,27 @@ var methodOverride = require('method-override');
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongo_port = process.env.MONGO_PORT || process.env.OPENSHIFT_MONGO_PORT || 27017,
-    mongo_ip = process.env.MONGO_IP || process.env.OPENSHIFT_MONGO_IP;
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
 
-var mongoURL = 'mongodb://127.0.0.1/test',
-    mongoURLLabel = 'mongodb://127.0.0.1/test';
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+      mongoUser = process.env[mongoServiceName + '_USER'];
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ':' + mongoPassword + '@';
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+  }
+}
 
 var db = null,
     dbDetails = new Object();
@@ -22,7 +38,6 @@ var initDb = function(callback) {
   if (mongoose == null) return;
 
   db = mongoose.createConnection(mongoURL);
-  console.log('Connected to MongoDB at: %s', mongoURL);
 
 };
 // attempt initial setup of DB
@@ -49,7 +64,6 @@ app.route('/api/todos')
        if (err) response.send(err);
        response.json(todos); // return all todos in JSON format
       });
-      console.log('after find');
     }
   })
   .post(function(request, response, next){
@@ -97,9 +111,9 @@ app.route('/api/todos/:todo_id')
 
 
 
-// initDb(function(err){
-//     console.log('Error connecting to Mongo. Message:\n'+err);
-//   });
+initDb(function(err){
+    console.log('Error connecting to Mongo. Message:\n'+err);
+  });
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
